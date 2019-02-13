f58029d788a76d4df188d77a1e519afcad1801d2
Tobias Hansson <tobhan15@student.hh.se>
2019-02-13 21:16:50 +0100

    Getting started

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 g++ main.cpp -O3 -s -std=c++17 -march=native ^
- -fno-exceptions -lsfml-graphics -lsfml-window -lsfml-system
+ -fno-exceptions -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-system -lopenal32
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <SFML/Audio.hpp>
-#include "../Gist/src/gist.cpp"
-#include <boost/preprocessor.hpp>
+#include <iostream>
+#include "../Gist/src/gist.h"
 
 int main(){
+    sf::SoundBuffer buffer;
+    if(!buffer.loadFromFile("canary.wav"))
+        return -1;
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+    sound.play();
+    sound.setPitch(0.5f);
+    while(1){sound.setPitch(sound.getPitch()*1.00000000001);}
     return 0;
 }
